f5bd3e1815a23bb18188b09ea06d0044b573e603
Santiago <dylansan03.17@gmail.com>
2023-06-29 18:24:32 -0500

    Fifth changes

--- src/perfil.jsx
+++ src/perfil.jsx
@@ -3,15 +3,36 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import './perfil.css'
-import { Route, Routes, Link} from 'react-router-dom'
+import { Route, Routes, Link } from 'react-router-dom'
 
 
 
 export const Inicio = () => {
     return (
         <div className='divbox1'>
-            <h3 className='tx1'>Perfil Personal</h3>
-            <p>Soy Tecnólogo en Gestión de Redes y Sistemas Teleinformáticos del Instituto Superior de Educación Rural (ISER).</p>
+            <div className="divbox5">
+                <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="/src/assets/1.png" class="d-block w-100" alt="..."/>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="/src/assets/2.png" class="d-block w-100" alt="..."/>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="/src/assets/3.png" class="d-block w-100" alt="..."/>
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
